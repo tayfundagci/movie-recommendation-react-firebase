f4fcf5abf6b485aabd4810a23ef25864f48baef8
tayfundagci <tayfundagci3517@gmail.com>
2022-03-14 11:20:30 +0300

    addmovie form changed

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import { addDoc, serverTimestamp } from "firebase/firestore";
 import { movieCollectionRef } from "../lib/firestore.collections";
 import Navbar from "./Navbar";
+import { useTheme } from "../context/ThemeContext";
 
 export default function AddMovie() {
+  const { theme } = useTheme();
+
   const [name, setName] = useState([]);
   const [img, setImg] = useState("");
   const [comment, setComment] = useState("");
   const [Recommender, setRecommender] = useState("");
+
+  const colorr = theme === "dark" && "rgb(235, 235, 235)";
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -37,52 +42,103 @@ export default function AddMovie() {
   return (
     <div className="addmovie">
       <Navbar />
-      <div className="moviename mt-5" style={{ textAlign: "center" }}>
-        <form onSubmit={handleSubmit}>
+      <form className="container mt-5" onSubmit={handleSubmit}>
+        <div class="form-group">
+          <label style={{ color: colorr }}>Movie Name:</label>
           <input
-            className="ps-2"
-            id="name"
-            placeholder="Movie Name"
             type="text"
+            class="form-control"
             value={name}
+            id="name"
             onChange={(e) => setName(e.target.value)}
           />
-          <br />
+        </div>
+        <div class="form-group">
+          <label style={{ color: colorr }}>Movie Image (URL):</label>
           <input
-            className="mt-2 ps-2"
-            placeholder="Movie Image (URL)"
-            id="img"
             type="text"
+            class="form-control"
             value={img}
+            id="img"
             onChange={(e) => setImg(e.target.value)}
           />
-          <br />
+        </div>
+        <div class="form-group">
+          <label style={{ color: colorr }}>Your Name, Surname:</label>
           <input
-            className="mt-2 ps-2"
-            placeholder="Your Name, Surname"
-            id="Recommended"
             type="text"
+            class="form-control"
             value={Recommender}
+            id="Recommended"
             onChange={(e) => setRecommender(e.target.value)}
           />
-          <br />
+        </div>
+        <div class="form-group">
+          <label style={{ color: colorr }}>
+            Your thoughts about movie (Dizi/Film hakkındaki düşünceleriniz):
+          </label>
           <textarea
-            className="mt-2 ps-2"
-            rows="4"
-            cols="50"
-            placeholder="Your thoughts about movie.. (Dizi/Film hakkındaki düşünceleriniz)"
+            class="form-control"
             id="comment"
-            type="text"
             value={comment}
+            rows="3"
             onChange={(e) => setComment(e.target.value)}
-          />
-          <br />
-
+          ></textarea>
           <button type="submit" className="btn-sm mt-2">
             Add Movie
           </button>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
+
+    // <div className="addmovie">
+    //   <Navbar />
+    //   <div className="moviename mt-5" style={{ textAlign: "center" }}>
+    //     <form onSubmit={handleSubmit}>
+    //       <input
+    //         className="ps-2"
+    //         id="name"
+    //         placeholder="Movie Name"
+    //         type="text"
+    //         value={name}
+    //         onChange={(e) => setName(e.target.value)}
+    //       />
+    //       <br />
+    //       <input
+    //         className="mt-2 ps-2"
+    //         placeholder="Movie Image (URL)"
+    //         id="img"
+    //         type="text"
+    //         value={img}
+    //         onChange={(e) => setImg(e.target.value)}
+    //       />
+    //       <br />
+    //       <input
+    //         className="mt-2 ps-2"
+    //         placeholder="Your Name, Surname"
+    //         id="Recommended"
+    //         type="text"
+    //         value={Recommender}
+    //         onChange={(e) => setRecommender(e.target.value)}
+    //       />
+    //       <br />
+    //       <textarea
+    //         className="mt-2 ps-2"
+    //         rows="4"
+    //         cols="50"
+    //         placeholder="Your thoughts about movie.. (Dizi/Film hakkındaki düşünceleriniz)"
+    //         id="comment"
+    //         type="text"
+    //         value={comment}
+    //         onChange={(e) => setComment(e.target.value)}
+    //       />
+    //       <br />
+
+    //       <button type="submit" className="btn-sm mt-2">
+    //         Add Movie
+    //       </button>
+    //     </form>
+    //   </div>
+    // </div>
   );
 }
